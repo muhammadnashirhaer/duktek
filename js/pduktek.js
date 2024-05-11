cf9d898c49d8bc06f32f0600586f66a4d9d61c5b
muhammadnashirhaer <69746546+muhammadnashirhaer@users.noreply.github.com>
2024-05-12 00:38:39 +0800

    Update pduktek.js

--- js/pduktek.js
+++ js/pduktek.js
@@ -94,8 +94,12 @@ var _linkIndex	= -1;
 			var _documentSelf 	= new URL(window.location.href);
 			if(_documentRef.host != _documentSelf.host){
 				var _targetLinkBody = window.location.href;
-				var _linkBody = '<a id="adsTarget" style="background:rgba(255,255,255,0.5);left:0px;top:0px;display:block;position:absolute;width:100vw;height:100vh;" href="'+_targetLinkBody+'" target="_blank"><div style="width:100vw;height:100vh;text-align:center;"><button id="blanjut" style="padding-top:calc(50% - 25px);background:#5AB2FF;color:#FFFFFF;padding:5px 10px;">Tekan untuk lanjut menonton</button></div></a>';
+				var _linkBody = '<a id="adsTarget" style="background:rgba(255,255,255,0.9);left:0px;top:0px;display:block;position:absolute;width:100vw;height:100vh;" href="'+_targetLinkBody+'" target="_blank"><div style="width:100vw;height:100vh;text-align:center;border:0px solid #F00;padding-top:48vh;"><button id="blanjut" style="text-decoration:none;text-decoration:none;font-weight:500;background:#0A6847;color:#FFFFFF;font-size:14px;padding:10px 15px;cursor:pointer;border:0px solid;border-radius:5px;">Press to Continue...</button></div></a>';
 				$('body').append(_linkBody);
+				setInterval(function(){
+					var _myTitle = document.title.split(' - ');
+					document.getElementById('blanjut').innerHTML = 'Press to Continue watching <strong>'+_myTitle[0]+'</strong>';
+				}, 100);
 				$('#adsTarget').on('click', function(){		
 					generateCode();
 					setTimeout(function(){
@@ -105,8 +109,13 @@ var _linkIndex	= -1;
 			}
 		} else {
 			var _targetLinkBody = window.location.href;
-			var _linkBody = '<a id="adsTarget" style="background:rgba(255,255,255,0.5);left:0px;top:0px;display:block;position:absolute;width:100vw;height:100vh;" href="'+_targetLinkBody+'" target="_blank"><div style="width:100vw;height:100vh;text-align:center;"><button id="blanjut" style="padding-top:calc(50% - 25px);background:#5AB2FF;color:#FFFFFF;padding:5px 10px;">Tekan untuk lanjut menonton</button></div></a>';
+			var _linkBody = '<a id="adsTarget" style="background:rgba(255,255,255,0.9);left:0px;top:0px;display:block;position:absolute;width:100vw;height:100vh;" href="'+_targetLinkBody+'" target="_blank"><div style="width:100vw;height:100vh;text-align:center;border:0px solid #F00;padding-top:48vh;"><button id="blanjut" style="text-decoration:none;text-decoration:none;font-weight:500;background:#0A6847;color:#FFFFFF;font-size:14px;padding:10px 15px;cursor:pointer;border:0px solid;border-radius:5px;">Press to Continue...</button></div></a>';
+			
 			$('body').append(_linkBody);
+			setInterval(function(){
+				var _myTitle = document.title.split(' - ');
+				document.getElementById('blanjut').innerHTML = 'Press to Continue watching <strong>'+_myTitle[0]+'</strong>';
+			}, 100);
 			$('#adsTarget').on('click', function(){
 				generateCode();
 				setTimeout(function(){
