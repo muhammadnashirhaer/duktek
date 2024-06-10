e12b9ebccd4e792bef0ca978aae3c2b56c2abc8b
muhammadnashirhaer <69746546+muhammadnashirhaer@users.noreply.github.com>
2024-06-10 21:11:16 +0800

    Update pduktek16.js

--- js/pduktek16.js
+++ js/pduktek16.js
@@ -2,7 +2,7 @@ $('body').append('<div class="ad-box" style="position:fixed;top:0;left:0;" aria-
 var adBoxEl 		= document.querySelector(".ad-box")
 var hasAdBlock 		= window.getComputedStyle(adBoxEl)?.display === "none"		
 var _cData 		= 'en';
-var _enableAds 		= false;
+var _enableAds 		= true;
 $.get('https://1.1.1.1/cdn-cgi/trace', {}, function(_txtData){
 	var _lines = _txtData.split("\n");
 	for(var _x=0,_len=_lines.length;_x<_len;_x++){
@@ -71,7 +71,7 @@ var _linkIndex	= -1;
 		var _links			= [			
 			'https://sauptowhy.com/4/3184875',
 			'https://nv3tosjqd.com/2017599/',
-			'https://spokentomatoestraumatic.com/h9wy87em5h?key=20cb5df32b0cb6d71e91c2556d91f27a',			
+			'https://warlockstudent.com/h9wy87em5h?key=20cb5df32b0cb6d71e91c2556d91f27a',			
 		];
 		
 	var _directLinkUrl = '';	
